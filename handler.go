faacacac2a4dbd0570b39003ca03f4cadee77784
Justinas Stankevičius <justinas@justinas.me>
2013-08-23 23:31:01 +0300

    Basic token regenerating (cookie setting) for requests

--- handler.go
+++ handler.go
@@ -14,12 +14,20 @@ const (
 	HeaderName = "X-CSRF-Token"
 	// the HTTP status code for the default failure handler
 	FailureCode = 400
+
+	// Max-Age for the default base cookie. 365 days.
+	DefaultMaxAge = 365 * 24 * 60 * 60
 )
 
 type CSRFHandler struct {
 	// Handlers that CSRFHandler wraps.
 	successHandler http.Handler
 	failureHandler http.Handler
+
+	// The base cookie that CSRF cookies will be built upon.
+	// This should be a better solution of customizing the options
+	// than a bunch of methods SetCookieExpiration(), etc.
+	baseCookie http.Cookie
 
 	// Slices of URLs that are exempt from CSRF checks.
 	// They can be specified by...
@@ -41,18 +49,39 @@ func defaultFailureHandler(w http.ResponseWriter, r *http.Request) {
 // Constructs a new CSRFHandler that calls
 // the specified handler if the CSRF check succeeds.
 func New(handler http.Handler) *CSRFHandler {
+	baseCookie := http.Cookie{}
+	baseCookie.MaxAge = DefaultMaxAge
+
 	csrf := &CSRFHandler{successHandler: handler,
 		failureHandler: http.HandlerFunc(defaultFailureHandler),
 		exemptURLs:     make([]string, 0),
 		exemptGlobs:    make([]string, 0),
 		exemptRegexps:  make([]*regexp.Regexp, 0),
+		baseCookie:     baseCookie,
 	}
 
 	return csrf
+}
+
+// Generates a new token, sets it on the given request and returns it
+func (h *CSRFHandler) RegenerateToken(w http.ResponseWriter, r *http.Request) string {
+	token := generateToken()
+
+	cookie := h.baseCookie
+	cookie.Name = CookieName
+	cookie.Value = token
+
+	http.SetCookie(w, &cookie)
+
+	return token
 }
 
 // Sets the handler to call in case the CSRF check
 // fails. By default it's defaultFailureHandler.
 func (h *CSRFHandler) SetFailureHandler(handler http.Handler) {
 	h.failureHandler = handler
+}
+
+func (h *CSRFHandler) SetBaseCookie(cookie http.Cookie) {
+	h.baseCookie = cookie
 }
--- handler_test.go
+++ handler_test.go
@@ -1,23 +1,37 @@
 package nosurf
 
 import (
+	"fmt"
 	"net/http"
 	"net/http/httptest"
+	"strings"
 	"testing"
 )
 
 func TestDefaultFailureHandler(t *testing.T) {
 	writer := httptest.NewRecorder()
-	req, err := http.NewRequest("GET", "http://doesntmatter.com/", nil)
-
-	if err != nil {
-		t.Fatal(err)
-	}
+	req := dummyGet()
 
 	defaultFailureHandler(writer, req)
 
 	if writer.Code != FailureCode {
 		t.Errorf("Wrong status code for defaultFailure Handler: "+
 			"expected %d, got %d", FailureCode, writer.Code)
+	}
+}
+
+func TestRegenerateTokenWorks(t *testing.T) {
+	hand := New(http.HandlerFunc(defaultFailureHandler))
+	writer := httptest.NewRecorder()
+
+	req := dummyGet()
+	token := hand.RegenerateToken(writer, req)
+
+	header := writer.Header().Get("Set-Cookie")
+	expected_part := fmt.Sprintf("csrf_token=%s;", token)
+
+	if !strings.Contains(header, expected_part) {
+		t.Errorf("Expected header to contain %v, it doesn't. The header is %v.",
+			expected_part, header)
 	}
 }
