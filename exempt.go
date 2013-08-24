118c3e7ec29e03cccf66c91b7552db1f50cf7641
Justinas Stankevičius <justinas@justinas.me>
2013-08-24 17:38:08 +0300

    Regexp-based exemption

--- exempt.go
+++ exempt.go
@@ -1,8 +1,10 @@
 package nosurf
 
 import (
+	"fmt"
 	_ "path"
-	_ "regexp"
+	"reflect"
+	"regexp"
 )
 
 // Checks if the given path is exempt from CSRF checks
@@ -10,6 +12,15 @@ func (h *CSRFHandler) IsExempt(path string) bool {
 	// Check the exact urls first (the most specific rule)
 	if sContains(h.exemptPaths, path) {
 		return true
+	}
+
+	// glob checking will go here
+
+	// finally, the regexps
+	for _, re := range h.exemptRegexps {
+		if re.MatchString(path) {
+			return true
+		}
 	}
 
 	return false
@@ -27,5 +38,33 @@ func (h *CSRFHandler) ExemptPath(path string) {
 func (h *CSRFHandler) ExemptPaths(paths ...string) {
 	for _, v := range paths {
 		h.ExemptPath(v)
+	}
+}
+
+// Exempts a regular expression string or a compiled *regexp.Regexp
+// and exempts URLs that match it from CSRF checks.
+//
+// If the given argument is neither of the accepted values,
+// or the given string fails to compile, ExemptRegexp() panics.
+func (h *CSRFHandler) ExemptRegexp(re interface{}) {
+	var compiled *regexp.Regexp
+
+	switch re.(type) {
+	case string:
+		compiled = regexp.MustCompile(re.(string))
+	case *regexp.Regexp:
+		compiled = re.(*regexp.Regexp)
+	default:
+		err := fmt.Sprintf("%v isn't a valid type for ExemptRegexp()", reflect.TypeOf(re))
+		panic(err)
+	}
+
+	h.exemptRegexps = append(h.exemptRegexps, compiled)
+}
+
+// A variadic argument version of ExemptRegexp()
+func (h *CSRFHandler) ExemptRegexps(res ...interface{}) {
+	for _, v := range res {
+		h.ExemptRegexp(v)
 	}
 }
--- exempt_test.go
+++ exempt_test.go
@@ -1,6 +1,7 @@
 package nosurf
 
 import (
+	"regexp"
 	"testing"
 )
 
@@ -35,5 +36,125 @@ func TestExemptPaths(t *testing.T) {
 
 	if hand.IsExempt(other) {
 		t.Errorf("%v is exempt, but it shouldn't be")
+	}
+}
+
+// This only tests that ExemptRegexp handles the argument correctly
+// The matching itself is tested by TestExemptRegexpMatching
+func TestExemptRegexpCall(t *testing.T) {
+	pattern := "^/[rd]ope$"
+
+	// case 1: a string
+	hand := New(nil)
+	hand.ExemptRegexp(pattern)
+
+	// String() returns the original pattern
+	got := hand.exemptRegexps[0].String()
+
+	if pattern != got {
+		t.Errorf("The compiled pattern has changed: expected %v, got %v",
+			pattern, got)
+	}
+
+	// case 2: a compiled *Regexp
+	hand = New(nil)
+	re := regexp.MustCompile(pattern)
+	hand.ExemptRegexp(re)
+
+	got_re := hand.exemptRegexps[0]
+
+	if re != got_re {
+		t.Errorf("The compiled pattern is not what we gave: expected %v, got %v",
+			re, got_re)
+	}
+
+}
+
+func TestExemptRegexpInvalidType(t *testing.T) {
+	arg := 123
+
+	defer func() {
+		r := recover()
+		if r == nil {
+			t.Error("The function didn't panic on an invalid argument type")
+		}
+	}()
+
+	hand := New(nil)
+	hand.ExemptRegexp(arg)
+}
+
+func TestExemptRegexpInvalidPattern(t *testing.T) {
+	// an unclosed group
+	pattern := "a(b"
+
+	defer func() {
+		r := recover()
+		if r == nil {
+			t.Error("The function didn't panic on an invalid regular expression")
+		}
+	}()
+
+	hand := New(nil)
+	hand.ExemptRegexp(pattern)
+}
+
+// The same as TestExemptRegexCall, but for the variadic function
+func TestExemptRegexpsCall(t *testing.T) {
+	// case 1: a slice of strings
+	hand := New(nil)
+	slice1 := []interface{}{"^/$", "^/accounts$"}
+	hand.ExemptRegexps(slice1...)
+
+	for i := range slice1 {
+		pat := hand.exemptRegexps[i].String()
+		got := slice1[i]
+		if pat != got {
+			t.Errorf("The compiled pattern has changed: expected %v, got %v", pat, got)
+		}
+	}
+
+	// case 2: a mixed slice
+	hand = New(nil)
+	slice2 := []interface{}{"^/$", regexp.MustCompile("^/accounts$")}
+	hand.ExemptRegexps(slice2...)
+
+	pat := slice2[0].(string)
+	got := hand.exemptRegexps[0].String()
+	if pat != got {
+		t.Errorf("The compiled pattern has changed: expected %v, got %v", pat, got)
+	}
+
+	pat = slice2[1].(*regexp.Regexp).String()
+	got = hand.exemptRegexps[1].String()
+	if pat != got {
+		t.Errorf("The compiled pattern has changed: expected %v, got %v", pat, got)
+	}
+}
+
+func TestExemptRegexpMatching(t *testing.T) {
+	hand := New(nil)
+	re := `^/[mn]ail$`
+	hand.ExemptRegexp(re)
+
+	// valid
+	test := "/mail"
+	if !hand.IsExempt(test) {
+		t.Errorf("%v should be exempt, but it isn't.", test)
+	}
+
+	test = "/nail"
+	if !hand.IsExempt(test) {
+		t.Errorf("%v should be exempt, but it isn't.", test)
+	}
+
+	test = "/mail/outbox"
+	if hand.IsExempt(test) {
+		t.Errorf("%v shouldn't be exempt, but it is.", test)
+	}
+
+	test = "/snail"
+	if hand.IsExempt(test) {
+		t.Errorf("%v shouldn't be exempt, but it is.", test)
 	}
 }
