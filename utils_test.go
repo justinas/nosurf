e971e87c26b8d67f17abfcdfedf23baf76a9a993
Justinas Stankevičius <justinas@justinas.me>
2013-08-25 13:37:46 +0300

    A helper function for comparing Origins

--- utils_test.go
+++ utils_test.go
@@ -1,6 +1,7 @@
 package nosurf
 
 import (
+	"net/url"
 	"testing"
 )
 
@@ -16,4 +17,46 @@ func TestsContains(t *testing.T) {
 	if !sContains(slice, s2) {
 		t.Error("sContains said that %v contains %v, but it doesn't.", slice, s2)
 	}
+}
+
+func TestsameOrigin(t *testing.T) {
+	// a little helper that saves us time
+	p := func(rawurl string) *url.URL {
+		u, err := url.Parse(rawurl)
+		if err != nil {
+			t.Fatal(err)
+		}
+		return u
+	}
+
+	truthy := [][]*url.URL{
+		{p("http://dummy.us/"), p("http://dummy.us/faq")},
+		{p("https://dummy.us/some/page"), p("https://dummy.us/faq")},
+	}
+
+	falsy := [][]*url.URL{
+		// different ports
+		{p("http://dummy.us/"), p("http://dummy.us:8080")},
+		// different scheme
+		{p("https://dummy.us/"), p("http://dummy.us/")},
+		// different host
+		{p("https://dummy.us/"), p("http://dummybook.us/")},
+		// slightly different host
+		{p("https://beta.dummy.us/"), p("http://dummy.us/")},
+	}
+
+	for _, v := range truthy {
+		if !sameOrigin(v[0], v[1]) {
+			t.Errorf("%v and %v have the same origin, but sameOrigin() said otherwise.",
+				v[0], v[1])
+		}
+	}
+
+	for _, v := range falsy {
+		if sameOrigin(v[0], v[1]) {
+			t.Errorf("%v and %v don't have the same origin, but sameOrigin() said otherwise.",
+				v[0], v[1])
+		}
+	}
+
 }
