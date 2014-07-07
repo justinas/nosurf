fce30232cbd46b98caa3a4fdab6856bc5595cebb
Justinas Stankevicius <justinas@justinas.me>
2014-07-07 21:11:31 +0300

    Remove unnecessary recreation of map.

--- handler_test.go
+++ handler_test.go
@@ -327,14 +327,13 @@ func TestAddsVaryCookieHeader(t *testing.T) {
 }
 
 func TestClearsContextAfterTheRequest(t *testing.T) {
-	contextMap = make(map[*http.Request]*csrfContext)
 	hand := New(http.HandlerFunc(succHand))
 	writer := httptest.NewRecorder()
 	req := dummyGet()
 
 	hand.ServeHTTP(writer, req)
 
-	if len(contextMap) != 0 || contextMap[req] != nil {
+	if contextMap[req] != nil {
 		t.Errorf("The context entry should have been cleared after the request.")
 		t.Errorf("Instead, the context entry remains: %v", contextMap[req])
 	}
