cb8f85f14038b7cc387eea2238388562d9a087d2
Justinas Stankevicius <justinas@justinas.me>
2016-09-18 12:42:29 +0300

    Add test for token accessibility when using Context

--- handler_test.go
+++ handler_test.go
@@ -2,6 +2,7 @@ package nosurf
 
 import (
 	"bytes"
+	"context"
 	"fmt"
 	"io"
 	"mime/multipart"
@@ -174,6 +175,26 @@ func TestManualVerify(t *testing.T) {
 func TestContextIsAccessible(t *testing.T) {
 	// case 1: success
 	succHand := func(w http.ResponseWriter, r *http.Request) {
+		token := Token(r)
+		if token == "" {
+			t.Errorf("Token is inaccessible in the success handler")
+		}
+	}
+
+	hand := New(http.HandlerFunc(succHand))
+
+	// we need a request that passes. Let's just use a safe method for that.
+	req := dummyGet()
+	writer := httptest.NewRecorder()
+
+	hand.ServeHTTP(writer, req)
+}
+
+// Confusing test name. Tests that nosurf's context is accessible
+// when a request with golang's context is passed into Token().
+func TestContextIsAccessibleWithContext(t *testing.T) {
+	succHand := func(w http.ResponseWriter, r *http.Request) {
+		r = r.WithContext(context.WithValue(r.Context(), "dummykey", "dummyval"))
 		token := Token(r)
 		if token == "" {
 			t.Errorf("Token is inaccessible in the success handler")
